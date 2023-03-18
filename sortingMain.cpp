5ccec0989bb820c0ecb6724b9fe6a30297bf563b
Ziad Emad <115287958+1Ziad-Etman1@users.noreply.github.com>
2023-03-19 01:28:52 +0200

    Added the rest sort algorithms

--- sortingMain.cpp
+++ sortingMain.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-int main() 
+int main()
 
 {
     /*vector<int> sizes = {200, 500, 1000, 5000, 10000, 20000, 50000};
@@ -47,6 +47,47 @@ int main()
     sortlib::count_sort(arr2, n2);
     cout << "Count Sorted Array:" << endl;
     sortlib::print_array(arr2, n2);
+
+    cout<<"\n\n**********************************************\n\n";
+
+    int arr3[] = { 8, 7, 42, 9, 2, 65 };
+    int n3 = sizeof(arr3) / sizeof(arr2[0]);
+    sortlib::selection_sort(arr3, n3);
+    cout << "selection Sorted Array:" << endl;
+    sortlib::print_array(arr3, n3);
+
+    cout<<"\n\n**********************************************\n\n";
+
+    int arr4[] = { 2, 4, 64, 9, 7, 1 };
+    int n4 = sizeof(arr4) / sizeof(arr4[0]);
+    sortlib::insertion_sort(arr4, n4);
+    cout << "insertion Sorted Array:" << endl;
+    sortlib::print_array(arr4, n4);
+
+    cout<<"\n\n**********************************************\n\n";
+
+    int arr5[] = { 6, 2, 60, 35, 67, 3 };
+    int n5 = sizeof(arr5) / sizeof(arr5[0]);
+    sortlib::bubble_sort(arr5, n5);
+    cout << "Bubble Sorted Array:" << endl;
+    sortlib::print_array(arr5, n5);
+
+    cout<<"\n\n**********************************************\n\n";
+
+    int arr6[] = { 355, 6, 33, 3, 3, 9 };
+    int n6 = sizeof(arr6) / sizeof(arr6[0]);
+    sortlib::quick_sort(arr6, 0, n6-1);
+    cout << "quick Sorted Array:" << endl;
+    sortlib::print_array(arr6, n6);
+
+    cout<<"\n\n**********************************************\n\n";
+
+    int arr7[] = { 10, 7, 20, 9, 3, 5 };
+    int n7 = sizeof(arr7) / sizeof(arr7[0]);
+    sortlib::merge_sort(arr7, 0, n7-1);
+    cout << "Merge Sorted Array:" << endl;
+    sortlib::print_array(arr7, n7);
+
 
     cout<<"\n\n**********************************************\n\n";
 
--- sortlib.h
+++ sortlib.h
@@ -12,20 +12,20 @@
 using namespace std;
 
 
-namespace sortlib 
+namespace sortlib
 {
 
      // Shell sort
     template<typename T>
-    void shell_sort(T arr[], int n) 
+    void shell_sort(T arr[], int n)
     {
-        for (int gap = n/2; gap > 0; gap /= 2) 
+        for (int gap = n/2; gap > 0; gap /= 2)
         {
-            for (int i = gap; i < n; i++) 
+            for (int i = gap; i < n; i++)
             {
                 T temp = arr[i];
                 int j = i;
-                for (; j >= gap && temp < arr[j-gap]; j -= gap) 
+                for (; j >= gap && temp < arr[j-gap]; j -= gap)
                 {
                     arr[j] = arr[j-gap];
                 }
@@ -36,12 +36,12 @@ namespace sortlib
 
     // Count sort
     template<typename T>
-    void count_sort(T arr[], int size) 
+    void count_sort(T arr[], int size)
     {
         T max_element = arr[0];
-        for (int i = 1; i < size; i++) 
+        for (int i = 1; i < size; i++)
         {
-            if (arr[i] > max_element) 
+            if (arr[i] > max_element)
             {
                 max_element = arr[i];
             }
@@ -49,31 +49,186 @@ namespace sortlib
 
         vector<T> count(max_element + 1, 0);
 
-        for (int i = 0; i < size; i++) 
+        for (int i = 0; i < size; i++)
         {
             count[arr[i]]++;
         }
-        
-        for (int i = 1; i <= max_element; i++) 
+
+        for (int i = 1; i <= max_element; i++)
         {
             count[i] += count[i - 1];
         }
-        
+
         T* output = new T[size];
 
-         for (int i = size - 1; i >= 0; i--) 
+         for (int i = size - 1; i >= 0; i--)
          {
             output[count[arr[i]] - 1] = arr[i];
             count[arr[i]]--;
         }
 
-        for (int i = 0; i < size; i++) 
+        for (int i = 0; i < size; i++)
         {
             arr[i] = output[i];
         }
 
         delete[] output;
     }
+
+    template <class T>
+    int partition (T Array[], int Start, int End)
+    {
+        int p = Array[Start];
+        int i = Start - 1;
+        int j = End + 1;
+
+        while (true)
+        {
+            do
+            {
+                i++;
+            }
+            while (Array[i] < p);
+
+            do
+            {
+                j--;
+            }
+            while (Array[j] > p);
+
+            if (i >= j)
+            {
+                return j;
+            }
+
+            swap(Array[i], Array[j]);
+        }
+    }
+
+    template <class T>
+    void quick_sort (T Array[] , int Start , int End)
+    {
+        if (Start < End)
+        {
+            int pivot = partition (Array,Start,End);
+            quick_sort (Array,Start,pivot);
+            quick_sort (Array,pivot+1,End);
+        }
+    }
+
+    template <class T>
+    void Merge (T mainArr[], int Start, int Mid, int End)
+    {
+        int i, j, k;
+        int Size1, Size2;
+        Size1 = Mid - Start + 1;
+        Size2 = End - Mid;
+        T *array1 = new T[Size1];
+        T *array2 = new T[Size2];
+
+        for (i = 0; i < Size1; i++)
+            array1[i] = mainArr[Start+i];
+        for (j = 0; j < Size2; j++)
+            array2[j]= mainArr[Mid + 1 + j];
+
+        i = j = 0;
+        k = Start;
+
+        while (i < Size1 && j < Size2)
+        {
+            if (array1[i] < array2[j])
+            {
+                mainArr[k] = array1[i];
+                i++;
+            }
+            else
+            {
+                mainArr[k] = array2[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < Size1)
+        {
+            mainArr[k] = array1[i];
+            i++;
+            k++;
+        }
+        while (j < Size2)
+        {
+            mainArr[k] = array2[j];
+            j++;
+            k++;
+        }
+        delete [] array1;
+        delete [] array2;
+    }
+
+    template <class T>
+    void merge_sort (T mainArr[], int Start, int End)
+    {
+        if (Start < End)
+        {
+            int mid = (Start + End) / 2;
+            merge_sort(mainArr, Start, mid);
+            merge_sort(mainArr, mid+1, End);
+            Merge(mainArr, Start, mid, End);
+        }
+    }
+
+    template<typename T>
+    void insertion_sort(T arr[], int size)
+    {
+        for (int i=1,j ; i<size ;i++)
+        {
+
+            T temp = arr[i];
+            for (j=i ; j>0&&temp<arr[j-1] ;j--)
+            {
+                arr[j]=arr[j-1];
+            }
+            arr[j]=temp;
+        }
+    }
+
+
+    template<typename T>
+    void selection_sort(T arr[], int size)
+    {
+        int index=0;
+        for (int i = 0, j ; i < size ; i++)
+        {
+            int min = arr[i];
+            index = i;
+            for (j = i ; j < size ; j++)
+            {
+                if (arr[j]<min)
+                {
+                    min = arr[j];
+                    index = j;
+                }
+            }
+            if(index != i)
+                swap(arr[i], arr[index]);
+        }
+    }
+
+
+    template<typename T>
+    void bubble_sort(T arr[], int size)
+    {
+        for (int i=0 ; i<size-1 ;i++)
+        {
+            for (int j=0 ; j<size-i-1 ;j++)
+            {
+                if (arr[j]>arr[j+1])
+                {
+                    swap(arr[j],arr[j+1]);
+                }
+            }
+        }
+    }
+
 
     // Print sorted array
     template<typename T>
@@ -86,13 +241,14 @@ namespace sortlib
         cout << endl;
     }
 
-   
+
+
 
 
     // Test sorting time
 
    /*template<typename T>
-    void test_sort(vector<int> sizes) 
+    void test_sort(vector<int> sizes)
     {
         random_device rd;
         mt19937 gen(rd());
@@ -103,7 +259,7 @@ namespace sortlib
 
         //vector<int> sizes = {1000, 10000, 100000, 1000000};
 
-        for (int size : sizes) 
+        for (int size : sizes)
         {
             T* arr = new T[size];
             for (int i = 0; i < size; i++) {
@@ -141,7 +297,7 @@ namespace sortlib
 
             data_file << size << "," << shell_sort_time.count() << "," << count_sort_time.count() << "\n";
 
-            
+
 
             delete[] arr;
         }
@@ -152,7 +308,7 @@ namespace sortlib
     // Test sorting time
 
 /*template<typename T>
-void test_sort(vector<int> sizes) 
+void test_sort(vector<int> sizes)
 {
     random_device rd;
     mt19937 gen(rd());
@@ -161,7 +317,7 @@ void test_sort(vector<int> sizes)
     ofstream data_file("data.csv");
     data_file << "Size,Shell Sort,Count Sort\n";
 
-    for (int size : sizes) 
+    for (int size : sizes)
     {
         T* arr = new T[size];
         for (int i = 0; i < size; i++) {
@@ -209,6 +365,7 @@ void test_sort(vector<int> sizes)
             arr[i] = distr(gen);
         }
 
+        /*Shell Sort*/
         auto start = chrono::high_resolution_clock::now();
         sortlib::shell_sort(arr, size);
         auto end = chrono::high_resolution_clock::now();
@@ -219,6 +376,7 @@ void test_sort(vector<int> sizes)
         cout << "Time elapsed: " << shell_sort_time.count() << " microseconds\n";
         cout<<"\n\n**********************************************\n\n";
 
+        /*Count Sort*/
         start = chrono::high_resolution_clock::now();
         sortlib::count_sort(arr, size);
         end = chrono::high_resolution_clock::now();
@@ -229,7 +387,67 @@ void test_sort(vector<int> sizes)
         cout << "Time elapsed: " << count_sort_time.count() << " microseconds\n";
         cout<<"\n\n**********************************************\n\n";
 
-        data_file << size << "," << shell_sort_time.count() << "," << count_sort_time.count() << "\n";
+        /*Insertion Sort*/
+        start = chrono::high_resolution_clock::now();
+        sortlib::insertion_sort(arr, size);
+        end = chrono::high_resolution_clock::now();
+        chrono::duration<double, std::micro> insertion_sort_time = end - start;
+
+        cout << "Insertion Sort:" << endl;
+        cout << "Size: " << size << "\n";
+        cout << "Time elapsed: " << insertion_sort_time.count() << " microseconds\n";
+        cout<<"\n\n**********************************************\n\n";
+
+        /*Selection Sort*/
+        start = chrono::high_resolution_clock::now();
+        sortlib::selection_sort(arr, size);
+        end = chrono::high_resolution_clock::now();
+        chrono::duration<double, std::micro> selection_sort_time = end - start;
+
+        cout << "Selection Sort:" << endl;
+        cout << "Size: " << size << "\n";
+        cout << "Time elapsed: " << selection_sort_time.count() << " microseconds\n";
+        cout<<"\n\n**********************************************\n\n";
+
+        /*Bubble Sort*/
+        start = chrono::high_resolution_clock::now();
+        sortlib::bubble_sort(arr, size);
+        end = chrono::high_resolution_clock::now();
+        chrono::duration<double, std::micro> bubble_sort_time = end - start;
+
+        cout << "Bubble Sort:" << endl;
+        cout << "Size: " << size << "\n";
+        cout << "Time elapsed: " << bubble_sort_time.count() << " microseconds\n";
+        cout<<"\n\n**********************************************\n\n";
+
+        /*Merge Sort*/
+        start = chrono::high_resolution_clock::now();
+        sortlib::merge_sort(arr, 0, size-1);
+        end = chrono::high_resolution_clock::now();
+        chrono::duration<double, std::micro> merge_sort_time = end - start;
+
+        cout << "Merge Sort:" << endl;
+        cout << "Size: " << size << "\n";
+        cout << "Time elapsed: " << merge_sort_time.count() << " microseconds\n";
+        cout<<"\n\n**********************************************\n\n";
+
+        /*Quick Sort*/
+        start = chrono::high_resolution_clock::now();
+        sortlib::quick_sort(arr, 0, size-1);
+        end = chrono::high_resolution_clock::now();
+        chrono::duration<double, std::micro> quick_sort_time = end - start;
+
+        cout << "Quick Sort:" << endl;
+        cout << "Size: " << size << "\n";
+        cout << "Time elapsed: " << quick_sort_time.count() << " microseconds\n";
+        cout<<"\n\n**********************************************\n\n";
+
+        data_file << size << "," << shell_sort_time.count() << "," << count_sort_time.count() <<
+        quick_sort_time.count() << "," << merge_sort_time.count() <<
+        insertion_sort_time.count() << "," << selection_sort_time.count() <<
+        bubble_sort_time.count() <<  "\n";
+
+
 
         delete[] arr;
     }
@@ -239,7 +457,7 @@ void test_sort(vector<int> sizes)
 
 
 
-       
+
 } // namespace sortlib
 
 #endif // SORTLIB_H
